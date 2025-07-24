036fce0d3cf2a7c35bbecfb65093da8856e21ff3
Jan Jelinek <janjelinek.dev@gmail.com>
2025-07-24 17:55:21 +0300

    Add content collections for .mdx posts

---
+++ src/content.config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+const posts = defineCollection({
+  loader: glob({ pattern: ['**/post.mdx'], base: 'src/collections' }),
+  schema: z.object({
+      sequence: z.number(),
+      slug: z.string(),
+      title: z.string(),
+    // state: z.enum(['ongoing', 'complete']),
+  }),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { posts };
